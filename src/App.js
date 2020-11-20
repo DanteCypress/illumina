22afeb5ba6259cb205d0e919bdc5c967af1f5816
Dante Cypress <dantecypress@gmail.com>
2020-11-19 16:07:37 -0800

    Fetch Data implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesActions";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div>
       <h1>Header</h1>
